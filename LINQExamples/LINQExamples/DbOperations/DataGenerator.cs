7fb145010fa907d18b9f190f5eb5b2e320c91fc6
Emre-Gelen <emregeln@gmail.com>
2022-02-22 23:10:59 +0300

    Added linq examples.

---
+++ LINQExamples/LINQExamples/DbOperations/DataGenerator.cs
@@ -0,0 +1,54 @@
+﻿using LINQExamples.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExamples.DbOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize()
+        {
+            using (var context = new LinqDbContext())
+            {
+                if (context.Students.Any())
+                {
+                    return;
+                }
+                context.Students.AddRange(
+                    new Student()
+                    {
+                        StudentId = 1,
+                        Name = "Emre",
+                        Surname = "Gelen",
+                        ClassId = 1
+                    }, 
+                    new Student()
+                    {
+                        StudentId = 2,
+                        Name = "Deniz",
+                        Surname = "Arda",
+                        ClassId = 1
+                    }, 
+                    new Student()
+                    {
+                        StudentId = 3,
+                        Name = "Umut",
+                        Surname = "Arda",
+                        ClassId = 2
+                    },
+                    new Student()
+                    {
+                        StudentId = 4,
+                        Name = "Ayşe",
+                        Surname = "Fatma",
+                        ClassId = 2
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ LINQExamples/LINQExamples/DbOperations/LinqDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LINQExamples.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExamples.DbOperations
+{
+    public class LinqDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("LinqDatabase");
+        }
+    }
+}
