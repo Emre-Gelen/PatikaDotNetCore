49983dea361bb5a58b6a0220ecaf057cef9acb0a
Emre-Gelen <emregeln@gmail.com>
2022-02-23 22:08:54 +0300

    Revert "Revert "Added LINQ examples.""
    This reverts commit f20f2300026078309a673e585ccfd64917e44891.

---
+++ DotNetCore/DotNetCoreWebApi/BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,38 @@
+﻿using DotNetCoreWebApi.Common;
+using DotNetCoreWebApi.DBOperations;
+using DotNetCoreWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebApi.BookOperations.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public GetBooksQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public List<BooksViewModel> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(o => o.Id).ToList();
+            List<BooksViewModel> vm = bookList.Select(x=> new BooksViewModel { 
+                Title = x.Title,
+                PageCount = x.PageCount,
+                PublishDate = x.PublishDate.Date.ToString("MM-dd-yyyy"),
+                GenreName = ((GenreEnum)x.GenreId).ToString()
+            }).ToList();
+            return vm;
+        }
+
+        public class BooksViewModel
+        {
+            public string Title { get; set; }
+            public int PageCount { get; set; }
+            public string PublishDate { get; set; }
+            public string GenreName { get; set; }
+        }
+    }
+}
--- LINQExamples/LINQExamples/Entities/Student.cs
+++ LINQExamples/LINQExamples/Entities/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace LINQExamples.Entities
 {
     public class Student
     {
+        [DatabaseGenerated(databaseGeneratedOption:DatabaseGeneratedOption.Identity)]
         public int StudentId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
