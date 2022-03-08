205275e78a07ccd4078260c41d148f473908b739
EMRE GELEN <emregeln@gmail.com>
2022-03-08 17:48:52 +0300

    Added UpdateBook tests.

--- DotNetCore/DotNetCoreWebApi/Application/BookOperations/Commands/UpdateBook/UpdateBookCommandValidator.cs
+++ DotNetCore/DotNetCoreWebApi/Application/BookOperations/Commands/UpdateBook/UpdateBookCommandValidator.cs
@@ -12,6 +12,7 @@ namespace DotNetCoreWebApi.Application.BookOperations.Commands.UpdateBook
         {
             RuleFor(command => command.BookId).GreaterThan(0);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(4);
+            RuleFor(command => command.Model.AuthorId).GreaterThan(0);
             RuleFor(command => command.Model.GenreId).NotEmpty().WithMessage("Genre Id can not be empty.").GreaterThan(0).WithMessage("Genre Id must be greater than 0.");
         }
     }
---
+++ DotNetCore/WebApi.UnitTest/Application/BookOperations/Commands/UpdateBook/UpdateBookCommandTest.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using DotNetCoreWebApi.Application.BookOperations.Commands.UpdateBook;
+using DotNetCoreWebApi.DBOperations;
+using FluentAssertions;
+using System;
+using System.Linq;
+using WebApi.UnitTests.TestSetup;
+using Xunit;
+using static DotNetCoreWebApi.Application.BookOperations.Commands.UpdateBook.UpdateBookCommand;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.UpdateBook
+{
+    public class UpdateBookCommandTest : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UpdateBookCommandTest(CommonTestFixture testFixture)
+        {
+            _dbContext = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+        [Fact]
+        public void WhenNotExistIdIsGiven_InvalidOperationException_ShouldReturnError()
+        {
+            //Arrange : Preparing
+            int Id = 1000000;
+            UpdateBookCommand command = new UpdateBookCommand(_dbContext);
+            command.BookId = Id;
+
+            //Act : Running && Assert : Validation
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Book was not found.");
+        }
+        [Fact]
+        public void WhenValidInputsAreGiven_InvalidOperationException_ShouldReturn()
+        {
+            //Arrange : Preparing
+            UpdateBookCommand command = new UpdateBookCommand(_dbContext);
+            int bookId = _dbContext.Books.Max(m => m.Id);
+            UpdateBookModel model = new UpdateBookModel() { AuthorId = 2, GenreId = 5, Title = "Updated Title" };
+
+            command.BookId = bookId;
+            command.Model = model;
+
+            //Act : Running
+            FluentActions
+                .Invoking(() => command.Handle()).Invoke();
+
+            //Assert : Validation
+            var book = _dbContext.Books.SingleOrDefault(s => s.Title == model.Title);
+            book.Should().NotBeNull();
+            book.GenreId.Should().Be(model.GenreId);
+            book.AuthorId.Should().Be(model.AuthorId);
+        }
+    }
+}
